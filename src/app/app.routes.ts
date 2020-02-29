892f30ece4c5e81dc84ce0d7c30d0173f7704019
Andremachado82 <andrels.machado82@gmail.com>
2020-02-29 09:23:06 -0300

    Projeto meat-app

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { NotFoundComponent } from './not-found/not-found.component';
+import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { HomeComponent } from './home/home.component';
+import { Routes } from '@angular/router';
+
+export const ROUTES: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'restaurants', component: RestaurantsComponent},
+  {path: 'restaurants/:id', component: RestaurantDetailComponent,
+    children: [
+      {path: '', redirectTo: 'menu', pathMatch: 'full'},
+      {path: 'menu', component: MenuComponent},
+      {path: 'reviews', component: ReviewsComponent},
+    ]},
+  {path: 'order', loadChildren: './order/order.module#OrderModule'},
+  {path: 'order-sumary', component: OrderSumaryComponent},
+  {path: 'about', loadChildren: './about/about.module#AboutModule'},
+  {path: '**', component: NotFoundComponent}
+]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,29 @@
+import { NotificationService } from './messages/notification.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OrderService } from './../order/order.service';
+import { RestaurantsService } from './../restaurants/restaurants.services';
+import { ShoppingCartService } from './../restaurant-detail/shopping-cart/shopping-cart.service';
+
+import { RatingComponent } from './rating/rating.component';
+import { RadioComponent } from './radio/radio.component';
+import { InputComponent } from './input/input.component';
+import { SnackbarComponent } from './messages/snackbar/snackbar.component';
+
+@NgModule({
+  declarations: [InputComponent, RatingComponent, RadioComponent, SnackbarComponent],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
+  exports: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent,
+            CommonModule, FormsModule, ReactiveFormsModule]
+})
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [ ShoppingCartService, RestaurantsService, OrderService, NotificationService ]
+    }
+  }
+}
