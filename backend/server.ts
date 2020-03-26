371864f4d592966c8c83c1c2245d3f4deb0f97cf
Andremachado82 <andrels.machado82@gmail.com>
2020-03-26 11:54:04 -0300

    Utilizando HTTPS e Typescript

--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,18 @@
-const jsonServer = require("json-server");
+import * as jsonServer from 'json-server';
+import { Express } from 'express';
+
+import * as fs from 'fs';
+import * as https from 'https';
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 
 // Add custom routes before JSON Server router
-server.get("/echo", (req, res) => {
+server.get('/echo', (req, res) => {
   res.jsonp(req.query);
 });
 
@@ -15,7 +20,7 @@ server.get("/echo", (req, res) => {
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     req.body.createdAt = Date.now();
   }
   // Continue to JSON Server router
@@ -24,6 +29,12 @@ server.use((req, res, next) => {
 
 // Use default router
 server.use(router);
-server.listen(3000, () => {
-  console.log("JSON Server is running");
+
+const options = {
+  cert: fs.readFileSync('./backend/keys/cert.pem'),
+  key: fs.readFileSync('./backend/keys/key.pem')
+}
+
+https.createServer(options, server).listen(3001, () => {
+  console.log('JSON Server is running on https://localhost:3001');
 });
