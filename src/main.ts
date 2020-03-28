b8eb130b526723cb58e81dfab1e3ed3943d3330e
Andremachado82 <andrels.machado82@gmail.com>
2020-03-28 10:39:09 -0300

    Melhorias no Processo de Compilação

--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+// tslint:disable-next-line: comment-format
+platformBrowserDynamic().bootstrapModule(AppModule) //{ preserveWhitespaces: true });
